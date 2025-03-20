2d6f01feb7aed8ba6390612fd266a535bfcf92e2
ManuelaFerreira <manu.code24@gmail.com>
2025-03-20 19:53:08 -0300

    check

--- IC596-LPII/19-03/EX5.c
+++ IC596-LPII/19-03/EX5.c
@@ -8,5 +8,33 @@ int main(void);
 int
 main()
 {
+   
+   // Declarando as variáveis:
+   
+   int i, j, jogada1, jogada2;
+   char tabuleiro[3][3];
+   
+   // Mensagem inicial:
+
+   printf("Bem-vindo ao JOGO DA VELHA!!");
+   printf("Escolha um numero entre 1 e 2 para decidir quem começará a partida");
+   
+   printf("Por favor, Jogador 1:");
+   scanf("%d", &jogada1);
+   printf("Por favor, Jogador 2:");
+   scanf("%d", &jogada2);
+
+   // Gerando o tabuleiro...
+
+      for (i = 0; i < 3; i++)
+         
+        printf("_");
+         
+         for (  j = 0; j < 3; j ++)
+         {
+            printf("%c",tabuleiro[i][j]);
+         }
+         
+    return 0;
 
 }
