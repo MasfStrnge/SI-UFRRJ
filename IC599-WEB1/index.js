be9f1ad9fff53d71e413a5edeb2fc5ea4fc0e41d
ManuelaFerreira <manu.code24@gmail.com>
2025-04-11 14:14:47 -0300

    ok

--- IC599-WEB1/index.js
+++ IC599-WEB1/index.js
@@ -1,8 +1,19 @@
-const http = require('http');
+const http = require('http'); // Importando o módulo http
 const server = http.createServer((req, res) => {
-console.log(req.url);
-res.end('Olá turma de WEB1 no Node js');
+const { url } = req;
+console.log(url);
+if (url === '/') {
+res.end('Oi, turma de Web 1');
+} else if (url === '/contato') {
+res.end('Esta é a página de contato');
+} else if (url === '/atividades') {
+res.end('Nossas atividades estão descritas aqui');
+} else {
+res.writeHead(404);
+res.end('A página não foi encontrada');
+}
 });
+// Escutando na porta 3000
 server.listen(3000, () => {
-console.log('Server running on port 3000');
+console.log('Servidor rodando em: http://localhost:3000');
 });
