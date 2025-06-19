ad5155f7793d4a55192c328a0d043f1f124f442f
ManuelaFerreira <manu.code24@gmail.com>
2025-06-19 10:43:03 -0300

    .

--- IC596-LPII/18-06_FILES/ex3_arq3.c
+++ IC596-LPII/18-06_FILES/ex3_arq3.c
@@ -1,6 +1,11 @@
 // 3. Pegue a letra de uma música qualquer e salve em um arquivo de texto. 
 // Depois, faça um programa que leia esse arquivo e escreva em outro a letra da música contendo apenas as linhas pares.
 
+// Abre um arquivo com a letra de uma música.
+// Copia apenas as linhas pares para outro arquivo.
+// Usa fgets para ler linha por linha e um contador para saber a posição.
+
+
 #include <stdio.h>
 
 int main(void)
@@ -16,9 +21,19 @@ int main(void)
     }
 
     char buffer[2000];
-    fgets(buffer,2000,letra_musica);
-    printf("%s\n", buffer);
+    int contador = 0;
+    letra_linhas_pares = fopen("nirvana_pares.txt","w");
 
+
+    while(fgets(buffer,2000,letra_musica)) {
+        contador++;
+
+        if (contador % 2 == 0) {
+            fprintf(letra_linhas_pares,buffer);
+        }
+    }
+    
+    fclose(letra_linhas_pares); 
     fclose(letra_musica);
 
     return 0;
