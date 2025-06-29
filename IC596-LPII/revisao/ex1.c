b869ebd0882c2340adb071d1562cd60df8e9a0a5
ManuelaFerreira <manu.code24@gmail.com>
2025-06-29 07:12:48 -0300

    .

---
+++ IC596-LPII/revisao/ex1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int tam = 1;
+    int *ponteiro = malloc(tam * sizeof(int));
+
+    if (ponteiro == NULL) {
+        printf("ERRO: FALHA NA ALOCAÇÃO");
+        return 1;
+    }
+
+    *ponteiro = 3;
+
+    printf("%d\n",*ponteiro);
+
+    return 0;
+}
